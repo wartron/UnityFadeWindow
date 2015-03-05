f97e5b8f8b295fba59cb90b3bf0c5d63b47f4ebc
Will Wharton <willmwharton@gmail.com>
2015-03-05 12:42:25 -0800

    first

---
+++ Assets/FadeWindow/Editor/FadeWindow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class FadeWindow : EditorWindow
+{
+
+    float fadeVal = 0.5f;
+
+    string tintPrefKey = "Playmode tint";
+
+    [MenuItem("Fade Window/Fade Slider")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(FadeWindow));
+    }
+
+    void OnGUI()
+    {
+        fadeVal = EditorGUILayout.Slider("Fade Amount", fadeVal, 0, 1);
+
+        //Debug.Log(EditorPrefs.GetString(tintPrefKey));
+
+        string oldTint = EditorPrefs.GetString(tintPrefKey);
+        string newTint = string.Format("Playmode tint;{0};{0};{0};1", fadeVal);
+
+        Debug.Log("old: " + oldTint);
+        Debug.Log("new: " + newTint);
+
+        EditorPrefs.SetString(tintPrefKey, newTint);
+
+    }
+}
